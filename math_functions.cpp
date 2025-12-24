f42075021c1424f48d404db55d18d1a2e896852d
Nikhil09-hub <bandarunikhil48@gmail.com>
2025-12-24 11:46:25 +0530

    Adding Math_Functions

---
+++ math_functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cmath>
+#include<string>
+int main(){
+    int x = 4;
+    int y = 5;
+    
+    std::cout <<  std::max(x,y) << '\n' ;
+    std::cout <<  std::min(x,y) << '\n';
+    std::cout <<  pow(4,5)<< '\n';
+    std::cout << sqrt(4) << '\n';
+    std::cout << round(4.6) << '\n';
+    std::cout << ceil(4.2) << '\n';
+    std::cout << floor(4.7) << '\n';
+    std::cout << abs(-6);
+
+
+    return 0;
+}
